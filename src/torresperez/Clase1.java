81ecd5272ffdc75097f2a3694347fffde4305cc5
dam <dam@203P29>
2024-03-07 09:44:18 +0100

    Se ha creado la rama clasenueva.

---
+++ src/torresperez/Clase1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package torresperez;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Soy Erasmo");
+        System.out.println("En un examen.");
+    }
+    
+}
