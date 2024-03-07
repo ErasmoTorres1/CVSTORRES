71ee9c746b3ff1e491787dc3d502daa4604b0e8c
dam <dam@203P29>
2024-03-07 09:53:58 +0100

    Se ha creado la rama clasenueva.

---
+++ src/torresperez/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package torresperez;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public static void main(String[] args) {
+        System.out.println("Estamos en la rama produccion");
+        System.out.println("En la clase 2.");
+    }
+}
